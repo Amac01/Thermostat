23b04d561f1efd2e0a7e4aa4464dcb82f6e54160
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2018-11-29 20:40:53 +0000

    Styled the The Interface

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,24 @@
-$(document).ready(function() {
-  var thermostat = new Thermostat();
+function displayWeather(city) {
+ var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+ var token = '&appid=c6cd4dbd5e70abfbc43e075ae7abdd14';
+ var units = '&units=metric';
+ $.get(url + token + units, function(data) {
+   $('#temperature').text(data.main.temp);
+ });
+}
+// c6cd4dbd5e70abfbc43e075ae7abdd14
+
+
+var thermostat = new Thermostat();
+
+displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+});
+
   updateTemperature();
 
 $('#temperature-up').on('click', function() { // event listener
@@ -17,20 +36,33 @@ $('#temperature-reset').click(function() { // alternate version of .on(click) wi
   updateTemperature(); // update view
 })
 
-$('#powersaving-on').click(function() { // alternate version of .on(click) with a sprinkle of jQuery syntactic sugar
-  thermostat.switchPowerSavingModeOn(); // update model
+$('#powersaving').click(function() { // alternate version of .on(click) with a sprinkle of jQuery syntactic sugar
+  updatePowerSaving();
+  // thermostat.switchPowerSavingModeOn(); // update model
   $('#power-saving').text('on')
   updateTemperature(); // update view
 })
 
-$('#powersaving-off').click(function() { // alternate version of .on(click) with a sprinkle of jQuery syntactic sugar
-  thermostat.switchPowerSavingModeOff(); // update model
-  $('#power-saving').text('off')
-  updateTemperature(); // update view
-})
+// $('#powersaving-off').click(function() { // alternate version of .on(click) with a sprinkle of jQuery syntactic sugar
+//   thermostat.switchPowerSavingModeOff(); // update model
+//   $('#power-saving').text('off')
+//   $('#powersaving-off').attr('value', 'PSM On');
+//   updateTemperature(); // update view
+// })
 
 function updateTemperature() {
   $('#temperature').text(thermostat.temperature);
   $('#temperature').attr('class', thermostat.energyUsage());
 }
-})
+
+function updatePowerSaving() {
+  if (thermostat.isPowerSavingModeOn()) {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off')
+    $('#powersaving').attr('value', 'Turn PSM On')
+  } else {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on')
+    $('#powersaving').attr('value', 'Turn PSM Off')
+  }
+}
